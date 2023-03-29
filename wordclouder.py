0a0d6e5a8a5d842fb35002920e29d97764703e3b
Bernardino Sassoli <sassoli@gmail.com>
2023-03-29 14:55:37 +0200

    modifies wordclouder.py

--- wordclouder.py
+++ wordclouder.py
@@ -4,12 +4,15 @@ from nltk.corpus import stopwords
 import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import nltk
+nltk.download('punkt')
+
 
 def make_worcloud(data: pd.DataFrame, max_words=100) -> plt.figure:
 
     reviews_text = (' ').join(data.iloc[:, 3].to_list())
     stop_words = set(stopwords.words('italian'))
-    stop_words |= {"tadino","gae aulenti", "concordia", "pandenus", "euro", "locale", "ristorante", "molto", "due", "tre", "stato", "stata", "stati", "sempre", "senza", "po"}
+    stop_words |= {"tadino", "gae aulenti", "concordia", "pandenus", "euro", "locale", "ristorante", "molto", "due", "tre", "stato", "stata", "stati", "sempre", "senza", "po"}
     word_tokens = word_tokenize(reviews_text)
     filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
     filtered_sentence = (' ').join(filtered_sentence)
